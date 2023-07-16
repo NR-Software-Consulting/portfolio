2ad9c90ac96c88c2f63ec7638bbed40d63e2e16a
Raza Mughal <razasabir23@outlook.com>
2023-07-16 16:12:57 +0500

    portfolio

--- app/components/about/styles.ts
+++ app/components/about/styles.ts
@@ -3,8 +3,6 @@ export const styles = {
     display: "flex",
     alignItems: "center",
     height: "100vh",
-    // paddingTop: "200px",
-    // paddingBottom: "200px",
   },
   MainGrid: {
     alignItems: "center",
